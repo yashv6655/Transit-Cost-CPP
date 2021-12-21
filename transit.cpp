f7a257be410c5bb878ddeb52463e52fac0e162ab
yashv6655 <yashv6655@gmail.com>
2021-12-21 00:26:53 -0500

    initial commit

---
+++ transit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    
+    int days, rides;
+    std::cout <<"Welcome to MTA Best Fare\n";
+
+
+    std::cout <<"How many days will you be using your MetroCard?\n";
+    
+    std::cin >> days;
+
+    std::cout <<"How many times do you expect to use the subway?\n";
+    
+    std::cin >> rides;    
+
+
+}
